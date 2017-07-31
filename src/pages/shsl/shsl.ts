fc21feab33fae812d14e653160bf7fa4df70932c
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2017-07-31 20:55:34 +0700

    Basic MockUp Done

--- src/pages/shsl/shsl.ts
+++ src/pages/shsl/shsl.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { HomePage } from '../home/home';
 /**
  * Generated class for the ShslPage page.
  *
@@ -15,11 +15,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ShslPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public alertCtrl: AlertController
+    ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ShslPage');
+  }
+
+
+  gotoHome(){
+    let confirm = this.alertCtrl.create({
+      title: 'Apakah Anda Yakin?',
+      subTitle: 'Pembatalan pemilihan akan menghapus pilihan yang sudah dipilih.',
+      buttons: [
+        {
+          text: 'Tidak',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Ya',
+          handler: () => {
+            console.log('Agree clicked'),
+            this.navCtrl.setRoot(HomePage);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 
 }
